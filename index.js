dd371cd923f4cfddce16484b282618ca6805378b
Owyn <Owyn@users.noreply.github.com>
2019-09-03 18:45:08 +0300

    def upd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const trashIds = require('./config.json').trashIds;
 
 module.exports = function trash_lootbeams(d)
 {
-	d.hook('S_SPAWN_DROPITEM', 6, (event) => {           
+	d.hook('S_SPAWN_DROPITEM', 8, (event) => {           
 		if (trashIds.includes(event.item))
 		{
 			event.item = 70068;
